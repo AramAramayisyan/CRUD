57317ccf3e4fba36172e54bbf4d666c54badfa8b
AramAramayisyan <aramaramayisyan71@gmail.com>
2025-06-18 16:43:24 +0400

    user functionality and products view only user

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,9 +22,7 @@ class ProductController extends Controller
     public function index(Request $request)
     {
         $data = $this->productService->index($request);
-//        dd($data);
         return view('index', compact('data'));
-
     }
 
     public function create()
